201bf11e2fd343ea7c0279a48fd0ca0b80c76e39
kkkkoki <koki_inoue45@icloud.com>
2023-03-03 06:26:08 +0900

    refactor(server.ts): envのプロパティ名変更

--- firebase/server.ts
+++ firebase/server.ts
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase-admin/firestore';
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(JSON.parse(process.env.FIREBASE_ADMIN_KEY as string)),
+    credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_CONFIG as string)),
     databaseURL: "https://next-demo-e72cf-default-rtdb.asia-southeast1.firebasedatabase.app"
   });
 
