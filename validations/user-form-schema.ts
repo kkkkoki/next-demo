1387475b490eee5ea007042de4c5a911bdec5219
kkkkoki <koki_inoue45@icloud.com>
2023-03-19 23:27:11 +0900

    fix: birthdayとgenderのvalidation修正、不良な箇所削除

--- validations/user-form-schema.ts
+++ validations/user-form-schema.ts
@@ -5,43 +5,19 @@ const NAME_MIN_LENGTH = 1;
 const NAME_MAX_LENGTH = 50;
 const EMAIL_MAX_LENGTH = 256;
 
-const GENDER_LIST = [
-  {
-    value: 'female',
-    label: '男性',
-  },
-  {
-    value: 'male',
-    label: '女性',
-  },
-  {
-    value: 'other',
-    label: 'その他',
-  },
-];
-
 export const userFormSchema = z.object({
   name: z
-    .string({
-      required_error: 'ユーザー名を入力してください',
-      invalid_type_error: '名前は文字列で入力してください',
-    })
-    .min(NAME_MIN_LENGTH, `${NAME_MIN_LENGTH}文字以上で入力してください`)
+    .string()
+    .min(NAME_MIN_LENGTH, '必須入力です')
     .max(NAME_MAX_LENGTH, `${NAME_MAX_LENGTH}文字以内で入力してください`),
   email: z
-    .string({
-      required_error: 'メールアドレスを入力してください',
-    })
+    .string()
     .max(EMAIL_MAX_LENGTH)
     .email('メールアドレスの形式が正しくありません'),
-  avatarUrl: z
-    .string({
-      required_error: 'アバター画像を選択してください',
-      invalid_type_error: '正しい形式の画像を選択してください',
-    })
-    .url('正しい形式の画像を選択してください'),
+  avatarUrl: z.string().url('正しい形式の画像を選択してください'),
   birthday: z
     .string()
+    .min(1, '必須入力です')
     .transform((val) => {
       const formatBirthday = (value: string, index: 4 | 7): string => {
         return value.slice(0, index) + '/' + value.slice(index);
@@ -61,11 +37,9 @@ export const userFormSchema = z.object({
         message: '生年月日が正しく入力されていません',
       }
     ),
-  gender: z
-    .string({ required_error: '性別を選択してください' })
-    .refine((val) => GENDER_LIST.map((gender) => gender.value).includes(val), {
-      message: '性別が正しく入力されていません',
-    }),
+  gender: z.enum(['female', 'male', 'other'], {
+    errorMap: (_issue, _ctx) => ({ message: '性別を選択してください' }),
+  }),
 });
 
 export type UserFormSchema = z.infer<typeof userFormSchema>;
